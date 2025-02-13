33e875e7c3e73a2e21dbaca49cd050ae7eb92290
Muhammad Abubakar Khalid <abubakarzzz07@gmail.com>
2025-02-13 00:00:51 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Model Function Calling / Tool Calling
+
+The purpose of this repo is to understand how function calling/tool calling works
+using google gemini models.
---
+++ hello.py
@@ -0,0 +1,10 @@
+from functions import model_config_none, model_config_auto, model_config_any, model_config_advanced_auto
+
+
+# model_config_advanced_auto("It's awful dark in here...")
+
+# model_config_any("Make this place PURPLE!")
+
+# model_config_auto("Light this place up!")
+
+model_config_none("Hello light-bot, what can you do?")
